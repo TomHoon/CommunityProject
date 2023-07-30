15aeff18f2d2a2daae84ed36f5bf31156e091c69
tomhoon <gnsdl9108@naver.com>
2023-07-30 15:25:02 +0900

    병현님 로그인,회원가입 붙이기

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router.js'
 import { createStore } from 'vuex'
-import Login from '@/components/Login'
 import GlobalJs from '@/js/global.js'
+
+// 전역 컴포넌트 등록
+import Login from '@/components/member/Login'
+import Agreement from '@/components/member/Agreement'
+import Join from '@/components/member/Join'
+
 
 const store = createStore({
     state() {
@@ -15,6 +20,10 @@ const store = createStore({
 
 const app = createApp(App);
 app.component('Login', Login);
+app.component('Agreement', Agreement);
+app.component('Join', Join);
+
+
 app.use(router);
 app.use(store);
 app.use(GlobalJs);
