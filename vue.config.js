1e923b5d0d444e2794e759e602fcddb4244159f5
tomhoon <gnsdl9108@naver.com>
2024-01-03 00:17:18 +0900

    nas주소추가

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,13 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave:false,
   devServer: {
+    // 💕nas
+    proxy: 'http://192.168.25.28:18080' 
+
     // 💕운영
-    proxy: 'http://gnsdl2846.cafe24.com' 
+    // proxy: 'http://gnsdl2846.cafe24.com' 
 
     // 💕개발
-    // proxy: 'http://localhost:8081' 
+    // proxy: 'http://localhost:8082' 
   }
 })
