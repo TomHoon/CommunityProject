8daa30b3fcd96e50dd9ff0ea2998c4c590d1a153
남병현 <kmi5063@naver.com>
2024-01-23 18:36:01 +0900

    쪽지 구현 중

---
+++ src/components/Modal_note_insert.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="modal" v-if="isOpen" @click="closeModalOutside">
+
+    <div class="modal-content" :style="{ width: width+'px', height: height+'px' }">
+      <i class="fa-solid fa-x modal-close" @click="closeModal"></i>
+      <slot></slot> <!-- 모달 내용을 slot으로 삽입합니다. -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    width: Number, // 너비를 문자열로 전달 받음
+    height: Number, // 높이를 문자열로 전달 받음
+    clickToClose: Boolean, // 딤처리 여부
+  },
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    modalOpen() {
+      this.isOpen = true;
+    },
+    closeModal() {
+      this.isOpen = false;
+    },
+    closeModalOutside(event) {
+      // 모달 바깥을 클릭하면 모달을 닫습니다.
+      if (event.target.classList.contains('modal')) {
+        if(this.clickToClose == true)
+          this.closeModal();
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* 모달 스타일링 */
+.modal {
+  /* 스타일링 내용 추가 */
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* 모달 배경의 반투명 검은색 배경 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999; /* 다른 요소 위에 표시 */
+}
+
+.modal-content {
+  /* 스타일링 내용 추가 */
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 20px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2); /* 그림자 효과 */
+  cursor: default;
+  border: #0d6efd 2px solid;
+
+}
+
+.modal-close{
+  width: 20px;
+  height: 20px;
+  transform: translateX(46rem);
+  background-color:transparent;
+  border: none;
+  cursor:pointer;
+}
+</style>
--- src/components/board/Board.vue
+++ src/components/board/Board.vue
@@ -3,7 +3,10 @@
   <div class="board-container">
     <div class="top-box">
       <button type="button" class="btn btn-primary write-btn" @click="goWrite">글쓰기</button>
-
+      <span class="message_image note_modalOpen" @mouseover="hovering = true" @mouseout="hovering = false"  @click="modal_note_list">
+        <img src="@/assets/icons8-message_white-48.png" v-show="!hovering" alt="쪽지함" class="message_white_btn" title="쪽지함">
+        <img src="@/assets/icons8-message_black-64.png" v-show="hovering" alt="쪽지함" class="message_black_btn" title="쪽지함">
+      </span>
       <button type="button" class="btn btn-primary right-btn" @click="changeBest">베스트</button>
       <button type="button" class="btn btn-primary right-btn" @click="changeCreateTime">날짜순</button>
 
@@ -59,10 +62,84 @@
       </div>
     </div>
   </div>
+
+<!-- 쪽지함리스트 -->
+  <Modal_note_list ref="modal_note_list" :width="900" :height="600"  :clickToClose=true>
+    <div class="note_list_area">
+      <div>
+        <span class="note_insert_area_title">쪽지함</span>
+      </div>
+      <div>
+        <select class="note_select" v-model="list.gubun" aria-label="Default select example">
+          <option value="받은쪽지함">받은쪽지함</option>
+          <option value="보낸쪽지함">보낸쪽지함</option>
+        </select>
+      </div>
+      <div>
+        <button class="modal_note_insert" @click="modal_note_insert">쪽지보내기</button>
+      </div>
+      <div class="note_list_chk_area">
+        <div class="note_list_check">
+          <input type="checkbox" id="note_list_allCheck" class="note_list_allCheck" name="note_list_allCheck">
+          <label for="note_list_allCheck">전체선택</label>
+        </div>
+        <span class="note_list_allNote">전체쪽지&nbsp; 개</span>
+        <span class="note_list_newNote">새쪽지&nbsp; 개 </span>
+      </div>
+      <div>
+        <table>
+          <tbody>
+            <tr v-for="(item, idx) in noteList" :key="idx" >
+              <td>
+                <strong>{{item.send_id}}</strong>
+              </td>
+            <td>
+                <strong>{{item.recv_id}}</strong>
+              </td>
+              <td>
+                <strong>{{item.note_title}}</strong>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </Modal_note_list>
+
+<!-- 쪽지쓰기 -->
+  <Modal_note_insert ref="modal_note_insert" :width="800" :height="500"  :clickToClose=true>
+    <div class="note_insert_area">
+      <span class="note_insert_area_title">쪽지보내기</span>
+    </div>
+    <div>
+      <ul class="note_insert_area_ul">
+        <li class="note_insert_area_li"> - &nbsp; 받는 아이디를 정확히 입력해주세요.</li>
+        <li class="note_insert_area_li"> - &nbsp; 보낸 후 취소는 불가합니다.</li>
+      </ul>
+    </div>
+    <div class="note_insert_area_input">
+      <div class="note_insert_title_area">
+        <input type="text" placeholder="제목을 입력하세요." v-model="insert.note_title" name="note_title" id="note_title" class="note_title">
+      </div>
+      <div class="send_id_area">
+        <input type="text" placeholder="받는아이디" v-model="insert.send_id" name="send_id" id="send_id" class="send_id" maxlength="13">
+        <button @click="sendIdCheck" class="sendIdCheck">아이디 확인</button>
+      </div>
+      <div class="note_content_area">
+        <textarea placeholder="내용을 입력하세요." v-model="insert.note_content" name="note_content" id="note_content" class="note_content" ></textarea>
+      </div>
+      <div>
+        <button class="note_btn" @click="note_btn">보내기</button>
+      </div>
+    </div>
+  </Modal_note_insert>
+
 </template>
 <script>
 import { getBoardAll, searchBoard, updateHitBoard } from '@/api/index'
 import comhubImg from '@/assets/comhub.png'
+import Modal_note_insert from '@/components/Modal_note_insert.vue';
+import Modal_note_list from '@/components/Modal_note_list.vue';
 
   export default {
     props: ['pageParams', 'transferObj'],
@@ -80,9 +157,23 @@ import comhubImg from '@/assets/comhub.png'
         bestToggle : false,
         searchData : '',
         default_image_path : comhubImg,
+        hovering: false,
+        insert: {
+          send_id: '',
+          note_title: '',
+          note_content: '',
+        },
+        list: {
+          gubun: '받은쪽지함',
+
+        },
       }
     },
     computed:{
+    },
+    components:{
+      Modal_note_insert,
+      Modal_note_list,
     },
     mounted() {
       this.$propsWatch();
@@ -220,7 +311,24 @@ import comhubImg from '@/assets/comhub.png'
           return item.image_path
         }
         return this.default_image_path
-      }
+      },
+      // 쪽지리스트 모달열기
+      modal_note_list() {
+        this.$refs.modal_note_list.modalOpen();
+      },
+      // 쪽지쓰기 모달열기
+      modal_note_insert() {
+        this.$refs.modal_note_insert.modalOpen();
+      },
+      sendIdCheck() {
+        alert("쪽지쓰기 - 아이디 확인")
+      },
+      note_btn() {
+        alert("쪽지쓰기 - 보내기")
+      },
+      note_list_send() {
+        alert("쪽지관리 - 쪽지쓰기")
+      },
     }
 }
 </script>
@@ -309,4 +417,138 @@ import comhubImg from '@/assets/comhub.png'
     margin-top:-1px;
     height: 30px;
   }
+  .message_image {
+    float: left;
+    margin-left: 20px;
+    width: 40px;
+    height: 40px;
+  }
+  .message_image:hover {
+    background-image: url('@/assets/icons8-message_black-64.png');
+  }
+  .message_white_btn {
+    width: 50px;
+    height: 55px;
+    transform: translate(-3px, -5px);
+  }
+  .message_black_btn {
+    transform: translate(-10px, -10px);
+  }
+  .note_insert_area_title {
+    color: #0d6efd;
+    font-size: 1.2rem;
+    font-weight: bold;
+    letter-spacing: 2px;
+  }
+  .note_insert_area_ul {
+    margin-top: 10px;
+  }
+  .note_insert_area_li {
+    text-align: left;
+    list-style: none;
+    font-size: 0.8rem;
+    line-height: 1.4rem;
+  }
+  .note_insert_area_input {
+    width: 700px;
+    margin: 0 auto;
+  }
+  .note_insert_title_area {
+    margin-bottom: 10px;
+  }
+  .note_title {
+    width: 650px;
+    height: 40px;
+    outline: none;
+    padding: 0 10px;
+    border: #c2c2c2 1px solid;
+
+  }
+  .send_id_area {
+    margin-bottom: 10px;
+  }
+  .send_id {
+    width: 500px;
+    margin-right: 60px;
+    height: 40px;
+    outline: none;
+    padding: 0 10px;
+    border: #c2c2c2 1px solid;
+  }
+  .sendIdCheck {
+    background-color: #0d6efd;
+    border: none;
+    color: rgb(237, 237, 237);
+    width: 90px;
+    height: 42px;
+    border-radius: 4px;
+    font-size: 14px;
+  }
+  .note_content {
+    width: 650px;
+    height: 160px;
+    resize: none;
+    outline: none;
+    padding: 10px;
+    border: #c2c2c2 1px solid;
+  }
+  .note_btn {
+    width: 652px;
+    height: 40px;
+    background-color: #0d6efd;
+    color: rgb(237, 237, 237);
+    border-radius: 4px;
+    border: none;
+    margin-top: 20px;
+  }
+  .note_list_area {
+    width: 730px;
+    margin: 0 auto;
+  }
+  // 쪽지리스트
+  .note_insert_area_title {
+    color: #0d6efd;
+    font-size: 1.2rem;
+    font-weight: bold;
+    letter-spacing: 2px;
+  }
+  .note_select {
+    width: 700px;
+    height: 40px;
+    padding-left: 10px;
+    outline: none;
+  }
+  .modal_note_insert {
+    width: 700px;
+    height: 40px;
+    background-color: #0d6efd;
+    color: rgb(237, 237, 237);
+    border-radius: 4px;
+    border: none;
+    margin-top: 15px;
+    font-weight: 600;
+    letter-spacing: 1px;
+
+  }
+  .note_list_chk_area{
+    margin-top: 30px;
+  }
+  .note_list_check {
+    float: left;
+
+  }
+  .note_list_allCheck {
+    margin-right: 5px;
+  }
+  .note_list_newNote {
+    float: right;
+    margin-right: 15px;
+  }
+  .note_list_allNote {
+    float: right;
+  }
+  .hr {
+    margin-top: 10px;
+  }
+
 </style>
