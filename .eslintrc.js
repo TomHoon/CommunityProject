e3197ba98ced22edcef14ecd4217b27d28173027
cet4713@gmail.com <cet4713@gmail.com>
2024-01-25 04:30:45 +0900

    fix: eslintrc.js의 parsing 에러 해결

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-    env: {
-      es6: true,
-      node: true
-    },
-    extends: "eslint:recommended",
-    globals: {
-      Atomics: "readonly",
-      SharedArrayBuffer: "readonly"
-    },
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: "module"
-    },
-    rules: {
-        semi: 0,
-        'import/no-unresolved': 'off',
-        'comma-dangle': 'off',
-        'no-new': 0,
-        indent: ['error', 2],
-        'import/extensions': 'off'
-    }
-  };
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: "eslint:recommended",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {
+    semi: 0,
+    'import/no-unresolved': 'off',
+    'comma-dangle': 'off',
+    'no-new': 0,
+    indent: ['error', 2],
+    'import/extensions': 'off'
+  }
+};
