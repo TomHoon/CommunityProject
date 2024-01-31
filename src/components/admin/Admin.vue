08e04547bd4a20b1095be8bfcdf2a290eaa2f236
cet4713@gmail.com <cet4713@gmail.com>
2024-01-31 21:38:35 +0900

    feat: 관리자 페이지

--- src/components/admin/Admin.vue
+++ src/components/admin/Admin.vue
@@ -1,6 +1,11 @@
 <template>
 <div>
     <h1 style="margin-top:100px">관리자 페이지</h1>
+    <div v-for="(item, idx) in boardData" :key="idx">
+        <span>[{{ item.gubun }}] </span>
+        <span>{{item.title}}</span>
+        <span>{{item.image_path}}</span>
+    </div>
 </div>  
 </template>
 
@@ -8,9 +13,18 @@
 import { getBoardAll } from '@/api/index'
 
 export default {
+    mounted() {
+        this.getBoardAll()
+    },
     data() {
         return {
-            
+            boardData : '',
+        }
+    },
+    methods: {
+        async getBoardAll() {
+            const res = await getBoardAll(0)
+            this.boardData = res.data
         }
     },
 }
