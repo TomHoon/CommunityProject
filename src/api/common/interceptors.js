28b441da7521df6882c52e555c537dfea437a768
cet4713@gmail.com <cet4713@gmail.com>
2024-02-06 17:03:06 +0900

    feat: axios interceptor token 검증 작업 추가

--- src/api/common/interceptors.js
+++ src/api/common/interceptors.js
@@ -1,10 +1,19 @@
 import store from '@/main'
+import { checkToken } from '@/api/index';
 
 export function setInterceptors(instance) {
   // Add a request interceptor
-  instance.interceptors.request.use(function (config) {
+  instance.interceptors.request.use(async function (config) {
     // Do something before request is sent
     config.headers.Authorization = store.state.token;
+
+    const res = await checkToken(store.state.id, store.state.token)
+    if(res.data !== "유효한 토큰입니다"){
+      alert("잘못된 토큰입니다")
+      // return Promise.reject(new Error("Invalid token"))
+      return new Promise(() => {})
+    }
+
     return config;
   }, function (error) {
     // Do something with request error
--- src/api/index.js
+++ src/api/index.js
@@ -40,73 +40,73 @@ function updateHitBoard(payload){
 }
 
 function sendList(payload){
-  return instance.post('/sendList', payload)
+  return axios.post('/sendList', payload)
 }
 
 function recvList(payload){
-  return instance.post('/recvList', payload)
+  return axios.post('/recvList', payload)
 }
 
 function findOneNote(payload){
-  return instance.post('/findOneNote', {note_idx:payload})
+  return axios.post('/findOneNote', {note_idx:payload})
 }
 
 function updateReadDate(payload){
-  return instance.post('/updateReadDate', {note_idx:payload})
+  return axios.post('/updateReadDate', {note_idx:payload})
 }
 
 function deleteRecv(payload){
-  return instance.post('/deleteRecv', {note_idx:payload})
+  return axios.post('/deleteRecv', {note_idx:payload})
 }
 
 function deleteSend(payload){
-  return instance.post('/deleteSend', {note_idx:payload})
+  return axios.post('/deleteSend', {note_idx:payload})
 }
 
 function insertNote(payload){
-  return instance.post('/insertNote', payload)
+  return axios.post('/insertNote', payload)
 }
 
 function findIdNote(payload){
-  return instance.post('/findIdNote', payload)
+  return axios.post('/findIdNote', payload)
 }
 
 function countReadYN(payload){
-  return instance.post('/countReadYN', {note_idx:payload})
+  return axios.post('/countReadYN', {note_idx:payload})
 }
 
 function countRecv(payload){
-  return instance.post('/countRecv', {note_idx:payload})
+  return axios.post('/countRecv', {note_idx:payload})
 }
 
 function countSend(payload){
-  return instance.post('/countSend', {note_idx:payload})
+  return axios.post('/countSend', {note_idx:payload})
 }
 
 function sendListChk(payload){
-  return instance.post('/sendListChk', payload)
+  return axios.post('/sendListChk', payload)
 }
 
 
 //BoardDetails.vue
 function getOneMember(payload){
-  return instance.post('/getOneMember', {member_id: payload})
+  return axios.post('/getOneMember', {member_id: payload})
 }
 
 function getOneFile(payload){
-  return instance.post('/getOneFile', {file_idx: payload})
+  return axios.post('/getOneFile', {file_idx: payload})
 }
 
 function getBoardById(payload){
-  return instance.post('/getBoardById', {id: payload})
+  return axios.post('/getBoardById', {id: payload})
 }
 
 function deleteBoard(payload){
-  return instance.post('/deleteBoard', {'id': payload})
+  return axios.post('/deleteBoard', {'id': payload})
 }
 
 function updateRecommendBoard(payload){
-  return instance.post('/updateRecommendBoard', payload)
+  return axios.post('/updateRecommendBoard', payload)
 }
 
 function getCommentByBoard(payload){
@@ -118,28 +118,28 @@ function addComment(payload){
 }
 
 function deleteComment(payload){
-  return instance.post('/deleteComment', {comment_idx : payload});
+  return axios.post('/deleteComment', {comment_idx : payload});
 }
 
 function recommendUpDown(payload){
-  return instance.post('/recommendUpDown', payload);
+  return axios.post('/recommendUpDown', payload);
 }
 
 //BoardModify.vue
 function updateBoard(payload){
-  return instance.post('/updateBoard', payload);
+  return axios.post('/updateBoard', payload);
 }
 
 
 //BoardWrite.vue
 function addBoard(payload){
-  return instance.post('/addBoard', payload, { headers: { 'Content-Type': 'multipart/form-data' } } )
+  return axios.post('/addBoard', payload, { headers: { 'Content-Type': 'multipart/form-data' } } )
 }
 
 
 //Join.vue
 function tempImg(payload){
-  return instance.post('/tempImg', payload, { headers: { 'Content-Type': 'multipart/form-data' } } )
+  return axios.post('/tempImg', payload, { headers: { 'Content-Type': 'multipart/form-data' } } )
 }
 
 function joinMember(payload){
@@ -149,25 +149,25 @@ function joinMember(payload){
 
 //Login.vue
 function loginMember(payload){
-  return instance.post("/loginMember", payload)
+  return axios.post("/loginMember", payload)
 }
 
 function joinOut(payload){
-  return instance.post('/joinOut', payload)
+  return axios.post('/joinOut', payload)
 }
 
 function memberUpdate(payload){
-  return instance.post("/memberUpdate", payload, { headers: { 'Content-Type': 'multipart/form-data' } } )
+  return axios.post("/memberUpdate", payload, { headers: { 'Content-Type': 'multipart/form-data' } } )
 }
 
 //admin
 function getMemberAll(){
-  return instance.get("/getMemberAll")
+  return axios.get("/getMemberAll")
 }
 
 //all
 function checkToken(id,token) {
-  return instance.post('/checkToken', {id:id, token:token})
+  return axios.post('/checkToken', {id:id, token:token})
 }
 
 
