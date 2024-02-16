c5a013d6def44a4d4309e6335e7dd980b83ae005
cet4713@gmail.com <cet4713@gmail.com>
2024-02-16 19:09:01 +0900

    feat: 관리자 로그인 설정

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { setInterceptors } from '@/api/common/interceptors'
+// import { setInterceptors } from '@/api/common/interceptors'
 
 // class Request {
 //     constructor() {
